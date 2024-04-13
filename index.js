426833cdaea719a6bb4f1ebf4ba0c9a4803fa834
Syed Ammar Qadir <saqadir@users.noreply.github.com>
2024-04-13 20:11:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
